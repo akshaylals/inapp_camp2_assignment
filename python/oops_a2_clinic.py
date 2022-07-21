ee4bcdcba5865b471f34b8273558055991d3772c
akshaylals <akshaylal.s@inapp.com>
2022-07-21 17:18:08 +0530

    assignment question

---
+++ python/oops_a2_clinic.py
@@ -0,0 +1,12 @@
+# Write a program to build a simple Clinic Management System using Python which
+# can perform following operations inside the class CMS
+# * Admit - This method takes patient details from the user like name,
+#   gender, age, dob, blood group. A patient_id should be automatically generated
+# * listPatients - This method displays the details of every patient.
+# * search - searches for a particular patient using patient_id
+# * Delete - deletes the record of a particular patient using patient_id
+# * Update - will ask for the patient_id and other details. It will replace
+#   the other details with new details.
+# Create inherited classes 'OP' and 'NonOP'. 'OP' should have the additional
+# Admit function and Non OP a function to generate an OP Ticket with 
+# ncrementing no for every patient
